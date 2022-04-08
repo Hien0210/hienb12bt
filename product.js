e0f2dac6b9ad154df60b4ff9b65cd87b47ca7e3c
Hien0210 <100427019+Hien0210@users.noreply.github.com>
2022-04-08 21:18:41 +0700

    a

---
+++ product.js
@@ -0,0 +1,203 @@
+const product =[
+    {
+        id: 'tissot1',
+        name: 'Tissot T109.610.36.032.00 Kính Sapphire',
+        priceold: '7.310.000',
+        pricenew: '5.848.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/236_T109.610.36.032.00.jpg',
+        img2: 'https://fridayshopping.vn/wp-content/uploads/2020/03/T109.610.36.032.00.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/03/hop-tissot-700x700.jpg',
+        category: 'tissot',
+    },
+    {
+        id: 'tissot2',
+        name: 'Tissot T063.210.11.057.00 Kính Sapphire',
+        priceold: '10.010.000',
+        pricenew: '8.508.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/10/36_T063.210.11.057.00-700x700.jpg',
+        img2: 'https://fridayshopping.vn/wp-content/uploads/2020/10/5_T063.210.11.057.00-700x700.jpg',
+        img3 : 'https://fridayshopping.vn/wp-content/uploads/2020/03/hop-tissot-700x700.jpg',
+        category: 'tissot',
+    },
+    {
+        id: 'tissot3',
+        name: 'Tissot T086.407.16.031.000 Bộ Máy Cơ ',
+        priceold: '23.130.000',
+        pricenew: '19.660.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/10/140_T086.407.16.031.00.jpg',
+        img2: 'https://fridayshopping.vn/wp-content/uploads/2020/10/TISSOT-T086.407.16.031.00-1-1-700x700.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/11/hop-tissot-699x699-3.jpg',
+        category: 'tissot',
+    },
+    {
+        id: 'tissot4',
+        name: 'Tissot T035.207.22.011.000  Bộ Máy Cơ',
+        priceold: '24.320.000',
+        pricenew: '20.672.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/47_T035.207.22.011.00-1-700x700.jpg',
+        img2: 'https://fridayshopping.vn/wp-content/uploads/2020/10/7_T085.207.22.011.00.jpg',
+        img3:'https://fridayshopping.vn/wp-content/uploads/2020/11/hop-tissot-699x699-3.jpg',
+        category: 'tissot',
+    },
+    {
+        id: 'tissot5',
+        name: 'Tissot T063.428.33.038.00 Kính Sapphire',
+        priceold: '25.290.000',
+        pricenew: '20.232.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/428_T063.428.33.038.00-3.jpg',
+        img2: 'https://fridayshopping.vn/wp-content/uploads/2020/03/428_T063.428.33.038.00-2-700x700.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/03/hop-tissot-700x700.jpg',
+        category: 'tissot',
+    },
+    {
+        id: 'longines1',
+        name: 'Longines L2.128.4.78.612.342  Máy Cơ',
+        priceold: '46.420.000',
+        pricenew: '39.457.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/10/27_L2.128.4.78.6-700x700.jpg',
+        img2: 'https://cdn.pnj.io/images/thumbnails/485/485/detailed/65/L2.128.4.87.6_Desktop_3.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/03/hop-longines1.jpg',
+        category: 'longines',
+    },
+    {
+        id: 'longines2',
+        name: 'Longines L4.709.2.21.7 Kính Sapphire',
+        priceold: '33.000.000',
+        pricenew: '28.050.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/10/20_L4.709.2.21.7-700x700.jpg',
+        img2: 'https://cdn.watchstore.vn/uploads/images/L4.709.2.12.8-1-1638156513339.jpg',
+        img3:'https://fridayshopping.vn/wp-content/uploads/2020/03/hop-longines1.jpg',
+        category: 'longines',
+    },
+    {
+        id: 'longines3',
+        name: 'Longines L4.89.44.72.64.5 Siêu Mỏng',
+        priceold: '26.620.000',
+        pricenew: '23.958.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/10/48_L4.859.4.72.6-700x700.jpg',
+        img2: 'https://www.thewatchcompany.com/media/catalog/product/cache/2e643380bf6acdbc360c33153b32ea2f/L/4/L49104126_1560887641.jpg',
+        img3:'https://fridayshopping.vn/wp-content/uploads/2020/03/hop-longines1.jpg',
+        category: 'longines',
+    },
+    {
+        id: 'longines4',
+        name: 'Longines L4.90.2.11.7.5.4  Bộ Máy Cơ',
+        priceold: '44.880.000',
+        pricenew: '35.904.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/163_L4.908.2.11.7-700x700.jpg',
+        img2: 'https://images.watchfinder.co.uk/imgv2/stock/204629/Longines-Presence-L4.720.2.12.7-204629-3-220201-143007.jpg;quality=90;h=425',
+        img3:'https://fridayshopping.vn/wp-content/uploads/2020/03/hop-longines1.jpg',
+        category: 'longines',
+    },
+    {
+        id: 'longines5',
+        name: 'Longines L2.128.4.78.3 Kính Sapphire',
+        priceold: '47.810.000',
+        pricenew: '40.300.000',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/10/28_L2.128.4.78.3-700x700.jpg',
+        img2: 'https://www.longines.com/media/catalog/product/cache/97f45038bd9818f82dbb29a471dd20cb/t/h/the-longines-master-collection-l2-128-4-78-3-detailed-view-2000x2000-101.jpg',
+        img3:'https://fridayshopping.vn/wp-content/uploads/2020/03/hop-longines1.jpg',
+        category: 'longines',
+    },
+    {
+        id: 'gshock1',
+        name: 'G-Shock GSTS330D, năng lượng mặt trời',
+        priceold: '9.870.000',
+        pricenew: '8.389.500',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/125_GST-S330D-1ADR-700x700.jpg',
+        img2: 'https://www.citywatches.ae/wp-content/uploads/2019/10/GST-S330D-1A_3.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/03/Casio_box.jpg',
+        category: 'gshock',
+    },
+    {
+        id: 'gshock2',
+        name: 'G-ShockBaby-G GG-B100-200, Bluetooth',
+        priceold: '10.528.000',
+        pricenew: '8.422.400',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/89_GG-B100-1A3DR-700x700.jpg',
+        img2: 'https://i.ppfocus.com/2020/8/534047b.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/03/Casio_box.jpg',
+        category: 'gshock',
+    },
+    {
+        id: 'gshock3',
+        name: 'G-Shock GST-S300G-1A2DR, World Time',
+        priceold: '8.202.000',
+        pricenew: '6.971.700',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/28_GST-S300G-1A2DR-700x700.jpg',
+        img2: 'https://www.lifestyle-collection.com.pk/wp-content/uploads/2020/07/gst-s300g-1a2dr-2.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/03/Casio_box.jpg',
+        category: 'gshock'
+    },
+    {
+        id: 'gshock4',
+        name: 'G-Shock GST-S120L, năng lượng mặt trời',
+        priceold: '8.648.000',
+        pricenew: '6.918.400',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/120_GST-S120L-1BDR-700x700.jpg',
+        img2: 'https://media.karousell.com/media/photos/products/2019/07/21/gsts120l1bcasiogshock_gsteel_1563705301_67d341c9_progressive.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/03/Casio_box.jpg',
+        category: 'gshock',
+    },
+    {
+        id: 'gshock5',
+        name: 'G-Shock Baby-G GA-400CS, World Time',
+        priceold: '4.489.000',
+        pricenew: '3.815.650',
+        img: 'https://fridayshopping.vn/wp-content/uploads/2020/03/21_GA-400CS-9ADR-700x700.jpg',
+        img2: 'https://media.karousell.com/media/photos/products/2016/09/05/authentic_brand_new_casio_gshock_ga400cs9a_crazy_color_green_yellow_ga400_ga400cs_ga400cs9_1473047473_d079e8a5.jpg',
+        img3: 'https://fridayshopping.vn/wp-content/uploads/2020/03/Casio_box.jpg',
+        category: 'gshock',
+    },
+]
+const item1 = localStorage.getItem("item1")
+? JSON.parse(localStorage.getItem("item1"))
+: [];
+const x = item1.length
+const finditem = product.find((u) =>
+       u.name ===item1[x-1].name1
+)
+console.log(finditem);
+console.log(item1);
+
+const container = document.querySelector('.container')
+const prd = document.createElement('div')
+prd.innerHTML = `<div class="imglist">
+                    <img src="`+finditem.img+`" class="mySlides " alt="">
+                    <img src="`+finditem.img2+`" class="mySlides" alt="">
+                    <img src="`+finditem.img3+`" class="mySlides" alt="">
+                    <a class="prev" onclick="plusSlides(-1)">❮</a>
+                    <a class="next" onclick="plusSlides(1)">❯</a>
+                    <br>                    
+                <div style="" class="dotlist">
+                      <img src="`+finditem.img+`" alt="" class="dot" onclick="currentSlide(1)">
+                     <img src="`+finditem.img2+`" alt="" class="dot" onclick="currentSlide(2)">
+                      <img src="`+finditem.img3+`" alt="" class="dot" onclick="currentSlide(3)"> 
+                </div>
+                </div>
+                <div class="product">
+                    <h1 class="name">`+finditem.name+`</h1>
+                    <div class="product-star">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                   </div>  
+                   <div class="product-price" style="padding-top: 30px;">
+                    <span class="product-priceold">`+finditem.priceold+`</span> <sup>đ</sup>
+                    <span class="product-pricenew">`+finditem.priceold+`</span> <sup>đ</sup>
+                   </div>
+                   <button class="addtocart">THÊM VÀO GIỎ HÀNG</button>
+                   <div class="col-inner">
+                    <p>Chất lượng chứng nhận bởi Hải Triều</p>
+                    <p>Bảo hành tại trung tâm bảo hành chính hãng</p>
+                    <p>Bộ sản phẩm: Hộp, phiếu bảo hành hãng</p>
+                    <p>Đổi trả 7 ngày nếu SP lỗi do nhà sản xuất</p>
+                    <p>Kiểm tra &amp; Vận chuyển tận nhà miễn phí</p>
+                    <p>Giao hàng HCM siêu tốc trong 2h</p>
+                   </div>
+                </div>`
+container.append(prd)
+prd.classList.add('main')
+
